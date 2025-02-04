31252414ecd9dada9e4d08c87bb0ef869f2cb00f
Matias D. Selvaggio <matias6selvaggio@gmail.com>
2025-02-04 14:36:46 +0000

    split delimiter

--- src/main.py
+++ src/main.py
@@ -3,11 +3,7 @@ from textnode import TextNode, TextType
 
 def main():
     text_node = TextNode("This is a text node",TextType.BOLD, "https://www.boot.dev")
-    print(text_node)
-
-
-
-            
+    print(text_node)            
 
 if __name__ == "__main__":
     main()
