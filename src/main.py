a6b3766ae48f2af09f7b238972104876d07267c3
Matias D. Selvaggio <matias6selvaggio@gmail.com>
2025-02-04 14:06:54 +0000

    text node to leaf node

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from textnode import TextNode, TextType
 
+
 def main():
     text_node = TextNode("This is a text node",TextType.BOLD, "https://www.boot.dev")
     print(text_node)
+
+
+
+            
 
 if __name__ == "__main__":
     main()
