696387a0bc49fbf28f5dc47639f12f662c6c9bff
Matias D. Selvaggio <matias6selvaggio@gmail.com>
2025-02-04 19:43:25 +0000

    block to html

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,16 @@
+import re 
+
+from htmlnode import *
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
 def markdown_to_blocks_hard_way(markdown):
     new_block = []
     
@@ -23,3 +36,143 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def block_to_block_type(block_markdown):
+
+    if validate_headings(block_markdown):
+        return block_type_heading
+    if validate_code(block_markdown):
+        return block_type_code
+    if validate_quote(block_markdown):
+        return block_type_quote
+    if validate_unordered_list(block_markdown):
+        return block_type_ulist
+    if validate_ordered_list(block_markdown):
+        return block_type_olist
+    return block_type_paragraph
+
+def validate_headings(word):
+    p = re.compile(r'#{1,6} [\s\S]{1,}')
+    return p.match(word)
+
+def validate_code(word):
+    p = re.compile(r"^```[\s\S]*?```$")
+    return p.match(word)
+
+def validate_quote(word):
+    split_lines = word.split("\n")
+    for line in split_lines:
+        if ( not line.startswith("> ")):
+            return False
+    return True
+
+def validate_unordered_list(word):
+    split_lines = word.split("\n")
+    for line in split_lines:
+        if ( not line.startswith("- ") and not line.startswith("* ")):
+            return False
+    return True
+
+def validate_ordered_list(word):
+    split_lines = word.split("\n")
+    count  = 1
+    for line in split_lines:
+        if ( not line.startswith(f"{count}. ")):
+            return False
+        count+=1
+    return True
+
+def markdown_to_html_node(markdown):
+    markdown_blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in markdown_blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+def block_to_html_node(block):   
+    type = block_to_block_type(block)
+    match(type):
+        case "heading":
+            return handle_heading(block) 
+        case "code":
+            return handle_code(block)
+        case "quote":
+            return handle_quote(block)
+        case "ordered_list":
+            return handle_olist(block)
+        case "unordered_list":
+            return handle_ulist(block)
+        case "paragraph":
+            return handle_paragraph(block)
+        case _:
+            raise Exception("invalid block type")
+     
+
+def handle_heading(block):
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"invalid heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
+    
+def handle_paragraph(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    tag = "p"
+    children = text_to_children(paragraph)
+    return ParentNode(tag, children)
+
+def handle_quote(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+
+def handle_code(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("invalid code block")
+    text = block[4:-3]
+    children = text_to_children(text)
+    tag = "pre"
+    code_node = ParentNode(tag="code", value=children)
+    return ParentNode(tag,[code_node])
+
+def handle_ulist(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+def handle_olist(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,8 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks, markdown_to_blocks_hard_way
+from htmlnode import HTMLNode
+
+from markdown_blocks import*
 
 class TestSplitDelimiter(unittest.TestCase):
 
@@ -66,6 +68,150 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_heading_valid(self):
+        text = "# Heading"
+        res = block_to_block_type(text)
+        self.assertEqual(
+            "heading",
+            res
+        )
+        text = "###### Heading"
+        res = block_to_block_type(text)
+        self.assertEqual(
+            "heading",
+            res
+        )
+    
+    def test_heading_invalid(self):
+        text = "#Heading"
+        res = block_to_block_type(text)
+        self.assertEqual(
+            "paragraph",
+            res
+        )
+        text = "####### Heading"
+        res = block_to_block_type(text)
+        self.assertEqual(
+            "paragraph",
+            res
+        )
+
+    def test_block_to_block_type_valid(self):
+        text = """> Quote
+> Quote2"""
+        res = block_to_block_type(text)
+        self.assertEqual(
+            "quote",
+            res
+        )
+        text = """- item
+- item2
+- item3"""
+        res = block_to_block_type(text)
+        self.assertEqual(
+            "unordered_list",
+            res
+        )
+        text = """* item
+* item2
+* item3"""
+        res = block_to_block_type(text)
+        self.assertEqual(
+            "unordered_list",
+            res
+        )
+        text = """1. item
+2. item2
+3. item3"""
+        res = block_to_block_type(text)
+        self.assertEqual(
+            "ordered_list",
+            res
+        )
+    ##aca 
+    ##aca
+    def test_paragraph(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with *italic* text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_lists(self):
+        md = """
+- This is a list
+- with items
+- and *more* items
+
+1. This is an `ordered` list
+2. with items
+3. and more items
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+        )
+
+    def test_headings(self):
+        md = """
+# this is an h1
+
+this is paragraph text
+
+## this is an h2
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        )
+
+    def test_blockquote(self):
+        md = """
+> This is a
+> blockquote block
+
+this is paragraph text
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
+        )
 
 if __name__ == "__main__":
     unittest.main()
